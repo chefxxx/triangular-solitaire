85a933573ed63c24e0d7a3e1d1762b4c2f78e963
chefxxx <mateusz.mm100@gmail.com>
2025-03-30 21:56:45 +0200

    Top level CmakeLists.txt complier flags added - temporary solution?

--- src/genetic_main.cpp
+++ src/genetic_main.cpp
@@ -2,9 +2,12 @@
 // Created by Mateusz Mikiciuk on 25/03/2025.
 //
 #include <iostream>
+#include <vector>
 
 int main()
 {
     std::cout << "Hello World!\n";
+    std::vector<int> v1{1,2,3,4};
+    auto v2 = 1;
     return 0;
 }
