07392044f3b21197607181831b18146cf1ba833b
chefxxx <mateusz.mm100@gmail.com>
2025-03-30 22:33:24 +0200

    genetic_alg first step

---
+++ src/genetic/genetic_alg.cpp
@@ -0,0 +1,6 @@
+//
+// Created by Mateusz Mikiciuk on 30/03/2025.
+//
+
+#include "includes/genetic_alg.h"
+
---
+++ src/genetic/includes/genetic_alg.h
@@ -0,0 +1,18 @@
+//
+// Created by Mateusz Mikiciuk on 30/03/2025.
+//
+
+#ifndef TRIANGULARSOLITAIRE_GENETIC_ALG_H
+#define TRIANGULARSOLITAIRE_GENETIC_ALG_H
+
+#include <vector>
+#include <functional>
+#include "chromosome.h"
+
+class genetic_alg
+{
+private:
+    std::vector<chromsome> chromosomes;
+};
+
+#endif //TRIANGULARSOLITAIRE_GENETIC_ALG_H
