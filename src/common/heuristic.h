7cc00cbd5d328c3e5d5f936122fe16d6e2ece7b3
Mykhailo Shamrai <mike.shamrai@gmail.com>
2025-05-08 11:40:27 +0200

    Added tuple of heuristic functors to struct Heuristics

--- src/common/heuristic.h
+++ src/common/heuristic.h
@@ -5,7 +5,6 @@
 #ifndef HEURISTIC_H
 #define HEURISTIC_H
 #include <board.h>
-#include <cstdint>
 #include <concepts>
 
 template<typename T>
@@ -13,5 +12,31 @@ concept Heuristic = requires(T t, const Board& b)
 {
     { t(b) } -> std::convertible_to<int>;
 };
+
+template<Heuristic... Hs>
+struct Heuristics
+{
+    // ------------------------------
+    // Class creation
+    // ------------------------------
+    explicit Heuristics(Hs... heuristics): heuristic_functions(std::make_tuple(std::move(heuristics)...)) {};
+    ~Heuristics() = default;
+
+    // ------------------------------
+    // Basic functions
+    // ------------------------------
+    auto calculate_heuristics(const Board& b)
+    {
+        return std::apply
+        ([&b](auto&&... hs)
+            {
+                return std::make_tuple(hs(b)...);
+            }, heuristic_functions
+        );
+    }
+    private:
+    std::tuple<Hs...> heuristic_functions;
+};
+
 
 #endif //HEURISTIC_H
--- tests/heuristics_test.cpp
+++ tests/heuristics_test.cpp
@@ -11,3 +11,16 @@ TEST(Heuristics, LambdaHeuristicCreation_SatisfiesConcept) {
 
     static_assert(Heuristic<decltype(lambda_heuristic)>);
 }
+
+TEST(Heuristics, StructHeuristicsCreation_Success)
+{
+    auto lambda_heuristic1 = [](Board b) { return static_cast<int>(1); };
+    auto lambda_heuristic2 = [](Board b) { return static_cast<int>(2); };
+    auto lambda_heuristic3 = [](Board b) { return static_cast<int>(3); };
+
+    Heuristics hs{lambda_heuristic1, lambda_heuristic2, lambda_heuristic3};
+    Board tmp{5};
+    auto res_tuple = hs.calculate_heuristics(tmp);
+
+    ASSERT_EQ(res_tuple, std::make_tuple(1, 2, 3));
+}
